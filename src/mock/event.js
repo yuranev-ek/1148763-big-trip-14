b7157d0232ee515b8493f71ac97f5260d33ae335
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 12:33:48 +0300

    mock(event): generate destination

--- src/mock/event.js
+++ src/mock/event.js
@@ -13,8 +13,11 @@ const ROUTE_TYPES = [
   'Restaurant',
 ];
 
+const CITIES = ['Chamonix', 'Paris', 'London', 'Geneva', 'Kazan', 'St. Petersburg'];
+
 export const generateEvent = () => {
   return {
     routeType: ROUTE_TYPES[getRandomInteger(0, ROUTE_TYPES.length - 1)],
+    destination: CITIES[getRandomInteger(0, CITIES.length - 1)],
   };
 };
