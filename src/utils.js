9e7a28a01b4821845a0b0d123b73167c98de0a1f
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 13:12:42 +0300

    utils: function to get random boolean

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,7 @@ export const getRandomInteger = (a = 0, b = 1) => {
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
+export const getRandomBoolean = () => {
+  return Boolean(getRandomInteger(0, 1));
+};
