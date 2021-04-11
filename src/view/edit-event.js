902b04af0fd57c17b794105557d47ec93274df7e
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 23:33:03 +0300

    feat(edit-event): rendered desc of destination

--- src/view/edit-event.js
+++ src/view/edit-event.js
@@ -133,7 +133,7 @@ export const createEditEventTemplate = (event) => {
 
             <section class="event__section  event__section--destination">
             <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-            <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
+            <p class="event__destination-description">${destination.description}</p>
             </section>
         </section>
         </form>
