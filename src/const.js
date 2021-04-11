e6e9e612ae0fe4fe9fe197db2925e2dc7495a86c
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 13:35:56 +0300

    const(lorem): added lorem and lorem sentences

---
+++ src/const.js
@@ -0,0 +1,4 @@
+const LOREM =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.';
+
+export const LOREM_SENTENCES = LOREM.split('. ');
