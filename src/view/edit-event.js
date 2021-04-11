ceb8b5b6989f24cc0bebdea5e8a648b3396d01dd
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 23:46:35 +0300

    feat(edit-event): rendered checked offers

--- src/view/edit-event.js
+++ src/view/edit-event.js
@@ -23,14 +23,32 @@ const createOptionsOfCities = (cities) => {
     .join('');
 };
 
+const createOffersTemplate = (offers) => {
+  return offers
+    .map((offer) => {
+      return `
+        <div class="event__offer-selector">
+            <input class="event__offer-checkbox  visually-hidden" id="event-${offer.title}" type="checkbox" name="event-${offer.title}" checked>
+            <label class="event__offer-label" for="event-${offer.title}">
+                <span class="event__offer-title">${offer.title}</span>
+                &plus;&euro;&nbsp;
+                <span class="event__offer-price">${offer.price}</span>
+            </label>
+        </div>
+        `;
+    })
+    .join('');
+};
+
 export const createEditEventTemplate = (event) => {
-  const { route, destination, basePrice, dateStart, dateEnd } = event;
+  const { route, destination, basePrice, dateStart, dateEnd, offers } = event;
 
   const srcToEventIcon = `img/icons/${route}.png`;
   const typeListOfRoutesTemplate = createTypeListOfRoutesTemplate(ROUTES);
   const optionsOfCitiesTemplate = createOptionsOfCities(CITIES);
   const formattedDateStart = formatDate(dateStart, DATE_FORMAT.DATE_TIME);
   const formattedDateEnd = formatDate(dateEnd, DATE_FORMAT.DATE_TIME);
+  const offersTemplate = createOffersTemplate(offers);
 
   return `
     <li class="trip-events__item">
@@ -85,59 +103,16 @@ export const createEditEventTemplate = (event) => {
         </header>
         <section class="event__details">
             <section class="event__section  event__section--offers">
-            <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+                <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
-            <div class="event__available-offers">
-                <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-                <label class="event__offer-label" for="event-offer-luggage-1">
-                    <span class="event__offer-title">Add luggage</span>
-                    &plus;&euro;&nbsp;
-                    <span class="event__offer-price">50</span>
-                </label>
+                <div class="event__available-offers">
+                    ${offersTemplate}
                 </div>
-
-                <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-                <label class="event__offer-label" for="event-offer-comfort-1">
-                    <span class="event__offer-title">Switch to comfort</span>
-                    &plus;&euro;&nbsp;
-                    <span class="event__offer-price">80</span>
-                </label>
-                </div>
-
-                <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-                <label class="event__offer-label" for="event-offer-meal-1">
-                    <span class="event__offer-title">Add meal</span>
-                    &plus;&euro;&nbsp;
-                    <span class="event__offer-price">15</span>
-                </label>
-                </div>
-
-                <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-                <label class="event__offer-label" for="event-offer-seats-1">
-                    <span class="event__offer-title">Choose seats</span>
-                    &plus;&euro;&nbsp;
-                    <span class="event__offer-price">5</span>
-                </label>
-                </div>
-
-                <div class="event__offer-selector">
-                <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-                <label class="event__offer-label" for="event-offer-train-1">
-                    <span class="event__offer-title">Travel by train</span>
-                    &plus;&euro;&nbsp;
-                    <span class="event__offer-price">40</span>
-                </label>
-                </div>
-            </div>
             </section>
 
             <section class="event__section  event__section--destination">
-            <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-            <p class="event__destination-description">${destination.description}</p>
+                <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+                <p class="event__destination-description">${destination.description}</p>
             </section>
         </section>
         </form>
