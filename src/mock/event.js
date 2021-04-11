e49611b4f8c24d443b4b77def61dadd8cf980e5b
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 14:13:48 +0300

    mock(event) generate pictures of destination

--- src/mock/event.js
+++ src/mock/event.js
@@ -21,6 +21,12 @@ const NUMBERS_OF_SENTENCES = {
   MAX: 5,
 };
 
+const PICTURE_META = {
+  MIN: 1,
+  MAX: 10,
+  SRC: 'http://picsum.photos/300/200?r=',
+};
+
 const generateDescription = (num) => {
   const SLICED_LOREM_SENTENCES = LOREM_SENTENCES.slice();
   const sentences = [];
@@ -34,12 +40,27 @@ const generateDescription = (num) => {
   return sentences.join('. ');
 };
 
+const generatePictures = (num) => {
+  const pictures = [];
+
+  while (num > pictures.length) {
+    pictures.push({
+      src: `${PICTURE_META.SRC}${Math.random()}`,
+      description: LOREM_SENTENCES[getRandomInteger(0, LOREM_SENTENCES.length - 1)],
+    });
+  }
+
+  return pictures;
+};
+
 const generateDestination = () => {
   const numbersOfSentences = getRandomInteger(NUMBERS_OF_SENTENCES.MIN, NUMBERS_OF_SENTENCES.MAX);
+  const numberOfPictures = getRandomInteger(PICTURE_META.MIN, PICTURE_META.MAX);
 
   return {
     name: CITIES[getRandomInteger(0, CITIES.length - 1)],
     description: generateDescription(numbersOfSentences),
+    pictures: generatePictures(numberOfPictures),
   };
 };
 
