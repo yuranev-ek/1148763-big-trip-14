d8cae6ffd33899dd2946cf723f27c31ce11948b6
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 23:29:40 +0300

    feat(edit-event): rendered base price

--- src/view/edit-event.js
+++ src/view/edit-event.js
@@ -70,7 +70,7 @@ export const createEditEventTemplate = (event) => {
                 <span class="visually-hidden">Price</span>
                 &euro;
             </label>
-            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="160">
+            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
             </div>
 
             <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
