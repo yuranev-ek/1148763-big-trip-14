fd6cc6b5dea2d7814be1bc9a81f1fff2271f3d62
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 23:21:18 +0300

    fix(eslint): fix rendering offers in event

--- src/view/event.js
+++ src/view/event.js
@@ -26,15 +26,15 @@ const diffConvertedTimeOfEvent = (dateEnd, dateStart) => {
 
 const createOffersTemplate = (offers) => {
   return offers
-    .map(
-      (offer) => `
+    .map((offer) => {
+      return `
       <li class="event__offer">
         <span class="event__offer-title">${offer.title}</span>
         &plus;&euro;&nbsp;
         <span class="event__offer-price">${offer.price}</span>
       </li>
-  `
-    )
+  `;
+    })
     .join('');
 };
 
