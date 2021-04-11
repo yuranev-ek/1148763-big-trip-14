c89eb9deaa8a64b21ab4acfa839b5610769164a1
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 23:22:47 +0300

    chore(main): added more events, passed event to edit-event, removed the new-event

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import { createSortTemplate } from './view/sort.js';
 import { createListOfEventsTemplate } from './view/list-of-events.js';
 import { createEditEventTemplate } from './view/edit-event.js';
 import { createEventTemplate } from './view/event.js';
-import { createNewEventTemplate } from './view/new-event.js';
 
 // mocks
 import { generateEvent } from './mock/event.js';
@@ -17,6 +16,9 @@ const render = (container, template, place = 'beforeend') => {
     container.insertAdjacentHTML(place, template);
   }
 };
+
+const EVENT_COUNT = 15;
+const events = new Array(EVENT_COUNT).fill().map(generateEvent);
 
 const siteHeaderElement = document.querySelector('.trip-main');
 render(siteHeaderElement, createRouteInformationTemplate(), 'afterbegin');
@@ -35,11 +37,6 @@ render(siteEventsElement, createSortTemplate());
 render(siteEventsElement, createListOfEventsTemplate());
 
 const siteListOfEventsTemplate = siteEventsElement.querySelector('.trip-events__list');
-render(siteListOfEventsTemplate, createEditEventTemplate());
+render(siteListOfEventsTemplate, createEditEventTemplate(events[0]));
 
-const EVENT_COUNT = 3;
-const events = new Array(EVENT_COUNT).fill().map(generateEvent);
-
-events.forEach((event) => render(siteListOfEventsTemplate, createEventTemplate(event)));
-
-render(siteListOfEventsTemplate, createNewEventTemplate());
+events.slice(1, events.length).forEach((event) => render(siteListOfEventsTemplate, createEventTemplate(event)));
