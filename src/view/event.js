f487db968ed8330f68e33c30399f2479150bd45b
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 13:14:15 +0300

    feat(event): rendering data in a template by isFavorite

--- src/view/event.js
+++ src/view/event.js
@@ -1,7 +1,8 @@
 export const createEventTemplate = (event) => {
-  const { routeType, destination } = event;
+  const { routeType, destination, isFavorite } = event;
 
   const srcToEventIcon = `img/icons/${routeType}.png`;
+  const classByIsFavorite = isFavorite ? 'event__favorite-btn--active' : '';
   return `
     <div class="event">
         <time class="event__date" datetime="2019-03-18">MAR 18</time>
@@ -28,10 +29,10 @@ export const createEventTemplate = (event) => {
             <span class="event__offer-price">200</span>
             </li>
         </ul>
-        <button class="event__favorite-btn  event__favorite-btn--active" type="button">
+        <button class="event__favorite-btn ${classByIsFavorite}" type="button">
             <span class="visually-hidden">Add to favorite</span>
             <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+                <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
             </svg>
         </button>
         <button class="event__rollup-btn" type="button">
