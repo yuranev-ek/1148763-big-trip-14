870ebda86e3c50487d188ef62d30e26ac0e903d0
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-11 23:38:41 +0300

    feat(edit-event): rendered dates

--- src/const.js
+++ src/const.js
@@ -6,6 +6,7 @@ export const LOREM_SENTENCES = LOREM.split('. ');
 export const DATE_FORMAT = {
   ATTR_DATE: 'DD-MM-YYYY',
   ATTR_DATE_TIME: 'DD-MM-YYYYTHH:mm',
+  DATE_TIME: 'DD/MM/YYYY HH:mm',
   TIME: 'HH:mm',
   DAY: 'MMM D',
 };
--- src/view/edit-event.js
+++ src/view/edit-event.js
@@ -1,4 +1,6 @@
 import { ROUTES, CITIES } from '../mock/event.js';
+import { formatDate } from '../utils.js';
+import { DATE_FORMAT } from '../const.js';
 
 const createTypeListOfRoutesTemplate = (routes) => {
   return routes
@@ -27,6 +29,8 @@ export const createEditEventTemplate = (event) => {
   const srcToEventIcon = `img/icons/${route}.png`;
   const typeListOfRoutesTemplate = createTypeListOfRoutesTemplate(ROUTES);
   const optionsOfCitiesTemplate = createOptionsOfCities(CITIES);
+  const formattedDateStart = formatDate(dateStart, DATE_FORMAT.DATE_TIME);
+  const formattedDateEnd = formatDate(dateEnd, DATE_FORMAT.DATE_TIME);
 
   return `
     <li class="trip-events__item">
@@ -58,25 +62,25 @@ export const createEditEventTemplate = (event) => {
             </div>
 
             <div class="event__field-group  event__field-group--time">
-            <label class="visually-hidden" for="event-start-time-1">From</label>
-            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 12:25">
-            &mdash;
-            <label class="visually-hidden" for="event-end-time-1">To</label>
-            <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 13:35">
+                <label class="visually-hidden" for="event-start-time-1">From</label>
+                <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${formattedDateStart}">
+                &mdash;
+                <label class="visually-hidden" for="event-end-time-1">To</label>
+                <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${formattedDateEnd}">
             </div>
 
             <div class="event__field-group  event__field-group--price">
-            <label class="event__label" for="event-price-1">
-                <span class="visually-hidden">Price</span>
-                &euro;
-            </label>
-            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
+                <label class="event__label" for="event-price-1">
+                    <span class="visually-hidden">Price</span>
+                    &euro;
+                </label>
+                <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
             </div>
 
             <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
             <button class="event__reset-btn" type="reset">Delete</button>
             <button class="event__rollup-btn" type="button">
-            <span class="visually-hidden">Open event</span>
+                <span class="visually-hidden">Open event</span>
             </button>
         </header>
         <section class="event__details">
