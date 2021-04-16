3ceee08fc0e1e8707ecf8df4ff52213718c54314
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-16 22:26:51 +0300

    refactor(const): put LOREM in event-mock since LOREM is used once

--- src/mock/event.js
+++ src/mock/event.js
@@ -1,6 +1,10 @@
 import { getRandomInteger, getRandomBoolean, subtractDays, addDays, getRandomPeriod } from '../utils.js';
-import { LOREM_SENTENCES } from '../const.js';
 import { generateOffers, OFFERS } from './offer.js';
+
+const LOREM =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.';
+
+const LOREM_SENTENCES = LOREM.split('. ');
 
 export const TYPE_OF_ROUTE = {
   TRIP: 'trip',
