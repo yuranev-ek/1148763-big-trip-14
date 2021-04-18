d570a59af17c7604e6c5f9deee4c00a3985497e4
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-18 23:20:09 +0300

    feat(no-events): implemented as a base class

---
+++ src/view/no-events.js
@@ -0,0 +1,13 @@
+import AbstractView from './abstract.js';
+
+const createNoEventTemplate = () => {
+  return `<p class="trip-events__msg">
+  Click New Event to create your first point
+  </p>`;
+};
+
+export default class NoEvents extends AbstractView {
+  getTemplate() {
+    return createNoEventTemplate();
+  }
+}
