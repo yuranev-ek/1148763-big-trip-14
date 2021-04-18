e74b5539fae030cc99d568eca7d98c8f792a8203
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-04-18 23:00:27 +0300

    feat(sort): implemented as a base class and rendered it

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // templates
 import { createRouteInformationTemplate } from './view/route-information.js';
-import { createSortTemplate } from './view/sort.js';
 import { createListOfEventsTemplate } from './view/list-of-events.js';
 import { createEditEventTemplate } from './view/edit-event.js';
 import { createEventTemplate } from './view/event.js';
 import TotalCostView from './view/total-cost.js';
 import MenuView from './view/menu.js';
 import FiltersView from './view/filters.js';
+import SortView from './view/sort.js';
 
 // mocks
 import { generateEvent } from './mock/event.js';
@@ -39,7 +39,7 @@ const siteFiltersElement = siteHeaderElement.querySelector('.trip-controls__filt
 renderElement(siteFiltersElement, new FiltersView().getElement(), RENDER_POSITION.BEFOREEND);
 
 const siteEventsElement = document.querySelector('.trip-events');
-renderTemplate(siteEventsElement, createSortTemplate());
+renderElement(siteEventsElement, new SortView().getElement(), RENDER_POSITION.BEFOREEND);
 renderTemplate(siteEventsElement, createListOfEventsTemplate());
 
 const siteListOfEventsTemplate = siteEventsElement.querySelector('.trip-events__list');
