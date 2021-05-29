58c6ca052946faca9600d00f2b85ad434f8dc9ca
Evgeniy.Yuranev <Evgeniy.Yuranev@tatar.ru>
2021-05-29 12:46:44 +0300

    fix(point-edit): fixed createOffersTemplate function -> hide block if !offers.length

--- src/view/point-edit.js
+++ src/view/point-edit.js
@@ -42,10 +42,11 @@ const createOptionsOfCities = (cities, isDisabled) => {
 };
 
 const createOffersTemplate = (currentOffers, offers = [], isDisabled) => {
+  let offersTemplate = null;
   const desiredOfferList = offers.find((offer) => offer.type === currentOffers.type);
 
-  if (desiredOfferList && desiredOfferList.offers) {
-    return desiredOfferList.offers
+  if (desiredOfferList && desiredOfferList.offers && desiredOfferList.offers.length) {
+    offersTemplate = desiredOfferList.offers
       .map((offer) => {
         const checked = currentOffers.list.findIndex((it) => it.title === offer.title) !== -1 ? 'checked' : '';
         return `
@@ -71,8 +72,18 @@ const createOffersTemplate = (currentOffers, offers = [], isDisabled) => {
         `;
       })
       .join('');
+
+    return `
+        <section class="event__section  event__section--offers">
+          <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+
+          <div class="event__available-offers">
+            ${offersTemplate}
+          </div>
+        </section>
+      `;
   } else {
-    return '<div class="event__offer-selector"></div>';
+    return '';
   }
 };
 
@@ -211,13 +222,7 @@ const createPointEditTemplate = (point) => {
             </button>
         </header>
         <section class="event__details">
-            <section class="event__section  event__section--offers">
-                <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
-                <div class="event__available-offers">
-                    ${offersTemplate}
-                </div>
-            </section>
+            ${offersTemplate}
 
             <section class="event__section  event__section--destination">
                 <h3 class="event__section-title  event__section-title--destination">Destination</h3>
